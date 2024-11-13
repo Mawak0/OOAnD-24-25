7b1e5f2cdf14bf1ab90c9141dab7b413284c0644
ShelDenis <shele1-2010@mail.com>
2024-11-13 22:10:21 +0600

    style: format vector

--- SpaceBattle.Lib.Tests/AngleTests.cs
+++ SpaceBattle.Lib.Tests/AngleTests.cs
@@ -8,13 +8,13 @@ public class AngleTests
     public void Execute_Adding()
     {
         var moqAngle1 = new Mock<IVector>();
-        moqAngle1.SetupGet(v => v.Values).Returns(new int[]{3, 5});
+        moqAngle1.SetupGet(v => v.Values).Returns(new int[] { 3, 5 });
 
         var moqAngle2 = new Mock<IVector>();
-        moqAngle2.SetupGet(v => v.Values).Returns(new int[]{4, 5});
+        moqAngle2.SetupGet(v => v.Values).Returns(new int[] { 4, 5 });
 
         var moqResAngle = new Mock<IVector>();
-        moqResAngle.SetupGet(v => v.Values).Returns(new int[]{2, 5});
+        moqResAngle.SetupGet(v => v.Values).Returns(new int[] { 2, 5 });
 
         var angle1 = new Angle(moqAngle1.Object);
         var angle2 = new Angle(moqAngle2.Object);
@@ -28,10 +28,10 @@ public class AngleTests
     public void Execute_AddingWithDifferentDenominators()
     {
         var moqAngle1 = new Mock<IVector>();
-        moqAngle1.SetupGet(v => v.Values).Returns(new int[]{3, 5});
+        moqAngle1.SetupGet(v => v.Values).Returns(new int[] { 3, 5 });
 
         var moqAngle2 = new Mock<IVector>();
-        moqAngle2.SetupGet(v => v.Values).Returns(new int[]{4, 7});
+        moqAngle2.SetupGet(v => v.Values).Returns(new int[] { 4, 7 });
 
         var angle1 = new Angle(moqAngle1.Object);
         var angle2 = new Angle(moqAngle2.Object);
@@ -43,7 +43,7 @@ public class AngleTests
     public void Execute_GetByIndex()
     {
         var moqAngle = new Mock<IVector>();
-        moqAngle.SetupGet(v => v.Values).Returns(new int[]{3, 5});
+        moqAngle.SetupGet(v => v.Values).Returns(new int[] { 3, 5 });
         var angle = new Angle(moqAngle.Object);
 
         Assert.Equal(3, angle[0]);
@@ -53,10 +53,10 @@ public class AngleTests
     public void Execute_Equal()
     {
         var moqAngle1 = new Mock<IVector>();
-        moqAngle1.SetupGet(v => v.Values).Returns(new int[]{3, 5});
+        moqAngle1.SetupGet(v => v.Values).Returns(new int[] { 3, 5 });
 
         var moqAngle2 = new Mock<IVector>();
-        moqAngle2.SetupGet(v => v.Values).Returns(new int[]{3, 5});
+        moqAngle2.SetupGet(v => v.Values).Returns(new int[] { 3, 5 });
 
         var angle1 = new Angle(moqAngle1.Object);
         var angle2 = new Angle(moqAngle2.Object);
@@ -68,10 +68,10 @@ public class AngleTests
     public void Execute_Equal_ThrowsException()
     {
         var moqAngle1 = new Mock<IVector>();
-        moqAngle1.SetupGet(v => v.Values).Returns(new int[]{3, 5});
+        moqAngle1.SetupGet(v => v.Values).Returns(new int[] { 3, 5 });
 
         var moqAngle2 = new Mock<IVector>();
-        moqAngle2.SetupGet(v => v.Values).Returns(new int[]{3, 7});
+        moqAngle2.SetupGet(v => v.Values).Returns(new int[] { 3, 7 });
 
         var angle1 = new Angle(moqAngle1.Object);
         var angle2 = new Angle(moqAngle2.Object);
@@ -83,10 +83,10 @@ public class AngleTests
     public void Execute_NotEqual()
     {
         var moqAngle1 = new Mock<IVector>();
-        moqAngle1.SetupGet(v => v.Values).Returns(new int[]{3, 5});
+        moqAngle1.SetupGet(v => v.Values).Returns(new int[] { 3, 5 });
 
         var moqAngle2 = new Mock<IVector>();
-        moqAngle2.SetupGet(v => v.Values).Returns(new int[]{4, 5});
+        moqAngle2.SetupGet(v => v.Values).Returns(new int[] { 4, 5 });
 
         var angle1 = new Angle(moqAngle1.Object);
         var angle2 = new Angle(moqAngle2.Object);
@@ -98,10 +98,10 @@ public class AngleTests
     public void Execute_NotEqual_ThrowsException()
     {
         var moqAngle1 = new Mock<IVector>();
-        moqAngle1.SetupGet(v => v.Values).Returns(new int[]{3, 5});
+        moqAngle1.SetupGet(v => v.Values).Returns(new int[] { 3, 5 });
 
         var moqAngle2 = new Mock<IVector>();
-        moqAngle2.SetupGet(v => v.Values).Returns(new int[]{3, 7});
+        moqAngle2.SetupGet(v => v.Values).Returns(new int[] { 3, 7 });
 
         var angle1 = new Angle(moqAngle1.Object);
         var angle2 = new Angle(moqAngle2.Object);
@@ -113,7 +113,7 @@ public class AngleTests
     public void Execute_AnglePrint()
     {
         var moqAngle = new Mock<IVector>();
-        moqAngle.SetupGet(v => v.Values).Returns(new int[]{3, 5});
+        moqAngle.SetupGet(v => v.Values).Returns(new int[] { 3, 5 });
 
         var angle = new Angle(moqAngle.Object);
 
