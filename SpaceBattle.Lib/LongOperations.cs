313457f7335db72878036e280c3610b563002446
Mawak <a32221580@gmail.com>
2024-11-14 16:42:23 +0600

    feat: RepeatCommand created

---
+++ SpaceBattle.Lib/LongOperations.cs
@@ -0,0 +1,18 @@
+﻿namespace SpaceBattle.Lib;
+
+internal class RepeatCommand : ICommand
+{
+    private readonly Queue _q;
+    private readonly ICommand _toRepeat;
+    public RepeatCommand(Queue q, ICommand toRepeat)
+    {
+        _q = q;
+        _toRepeat = toRepeat;
+    }
+
+    private void Execute()
+    {
+        _Queue.Add(_toRepeat);
+    }
+}
+
