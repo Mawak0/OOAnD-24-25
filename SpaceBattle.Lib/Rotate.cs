fe414aec8eae820312944d7105b4796210d06c15
ShelDenis <shele1-2010@mail.com>
2024-11-14 08:16:48 +0600

    chore: rotate update

--- SpaceBattle.Lib/Rotate.cs
+++ SpaceBattle.Lib/Rotate.cs
@@ -1,31 +1,19 @@
 ﻿namespace SpaceBattle.Lib;
 public interface IRotate
 {
-    Vector PositionAngle { get; set; }
-    Vector VelocityAngle { get; }
+    Angle PositionAngle { get; set; }
+    Angle VelocityAngle { get; }
 }
 
 public class RotateCommand : ICommand
 {
-    private readonly IRotate obj;
+    private IRotate obj;
     public RotateCommand(IRotate obj)
     {
         this.obj = obj;
     }
     public void Execute()
     {
-        obj.PositionAngle = new Vector{
-            obj.PositionAngle[0] + obj.VelocityAngle[0],
-            obj.PositionAngle[1] + 0,
-        };
-
-        obj.PositionAngle[0] = obj.PositionAngle[0] % obj.PositionAngle[1];
-        // double radians = Convert.ToDouble(2 * obj.PositionAngle[0] * 22) / Convert.ToDouble(obj.VelocityAngle[1] * 7);
-        // double degrees = Convert.ToDouble(obj.PositionAngle[0] * 360) / Convert.ToDouble(obj.VelocityAngle[1]);
-
-        // Console.WriteLine($"parts: {obj.PositionAngle[0]} / {obj.PositionAngle[1]}");
-        // Console.WriteLine($"radians: {radians}");
-        // Console.WriteLine($"degrees: {degrees}");
-
+        obj.PositionAngle += obj.VelocityAngle;
     }
 }
