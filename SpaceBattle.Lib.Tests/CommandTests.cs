f8b747f85401b01272082b0661ff9c3544d2244a
ShelDenis <144816515+ShelDenis@users.noreply.github.com>
2024-11-16 21:32:56 +0600

    chore: tests with empty command

--- SpaceBattle.Lib.Tests/CommandTests.cs
+++ SpaceBattle.Lib.Tests/CommandTests.cs
@@ -1,4 +1,4 @@
-﻿using Xunit;
+using Xunit;
 namespace SpaceBattle.Lib.Tests;
 
 public class CommandTests
@@ -7,7 +7,7 @@ public class CommandTests
     public void CommandStartTest()
     {
         var queue = new Queue();
-        var cmd = new AnyCommand();
+        var cmd = new EmptyCommand();
         var startCmd = new Start(ref queue, cmd);
         startCmd.Execute();
 
@@ -18,7 +18,7 @@ public class CommandTests
     public void CommandEndTest()
     {
         var queue = new Queue();
-        var cmd = new AnyCommand();
+        var cmd = new EmptyCommand();
         var startCmd = new Start(ref queue, cmd);
         startCmd.Execute();
         var endCmd = new End(ref queue);
@@ -31,7 +31,7 @@ public class CommandTests
     public void MCommandTest()
     {
         var queue = new Queue();
-        var cmd = new AnyCommand();
+        var cmd = new EmptyCommand();
         var mc = new MCommand(ref queue, cmd);
         mc.Execute();
 
