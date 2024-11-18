e72f4a85859697fb49d288f2d1c37d7a8c3f9ad9
Mawak0 <a32221580@gmail.com>
2024-11-18 21:01:07 +0600

    feat: started writing long operations

--- SpaceBattle.Lib/Move.cs
+++ SpaceBattle.Lib/Move.cs
@@ -1,4 +1,6 @@
-﻿namespace SpaceBattle.Lib;
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
 
 public interface IMoving
 {
@@ -16,5 +18,35 @@ public class MoveCommand : ICommand
     public void Execute()
     {
         obj.Position = obj.Position + obj.Velocity;
+    }
+}
+
+public interface StartMoveOrder {
+    object ObjectID {get; }
+    object GameID {get; }
+    Vector velocity {get; }
+}
+
+public class StartMoveCommand: ICommand{
+    private readonly StartMoveOrder _order;
+    public StartMoveCommand(StartMoveOrder order){
+        _order = order;
+    }
+    public void Execute(){
+        var gameObject = IoC.Resolve<object>("Game.Object.GetById", _order.ObjectID);
+        var setObjectVelocity = IoC.Resolve<ICommand>("Object.SetProperty", "velocity", _order.velocity);
+        if (setObjectVelocity is ICommand setObjectVelocityCommand){
+            setObjectVelocityCommand.Execute(); // !!!
+        }
+        if (gameObject is IMoving MovingObject){var c1 = new MoveCommand(MovingObject);}
+        var inj_obj = new InjectableMacroCommand();
+        var q_obj = IoC.Resolve<object>("Game.Queue");
+        if (q_obj is Queue queue){var c2 = new RepeatCommand(queue, inj_obj);}
+        var c3 = new MCommand(new List<ICommand> { c1, c2 });
+        if (inj_obj is Injectable inj) {inj.Inject(c3);}
+        
+        // +write inj to gameObject
+        
+         if (q_obj is Queue queue){queue.Add(inj);}
     }
 }
