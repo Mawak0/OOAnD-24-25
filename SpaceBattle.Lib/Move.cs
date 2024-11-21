281c17bd1dd8ca3099078cb18ed9e6fe3a9001c1
Anastasia Essina <aaessina@yandex.ru>
2024-11-21 12:17:47 +0600

    feat: LongOperations done

--- SpaceBattle.Lib/Move.cs
+++ SpaceBattle.Lib/Move.cs
@@ -1,11 +1,42 @@
-﻿using Hwdtech;
-
-namespace SpaceBattle.Lib;
+﻿namespace SpaceBattle.Lib;
 
 public interface IMoving
 {
     Vector Position { get; set; }
     Vector Velocity { get; }
+}
+
+public class MovingAdapter : IMoving
+{
+    private readonly IDictionary<string, object> _dictionary;
+
+    public MovingAdapter(IDictionary<string, object> dictionary)
+    {
+        _dictionary = dictionary;
+    }
+
+    public Vector Position
+    {
+        get
+        {
+            if (_dictionary.TryGetValue(nameof(Position), out var value) && value is Vector vector)
+                return vector;
+
+            return default;
+        }
+        set => _dictionary[nameof(Position)] = value;
+    }
+
+    public Vector Velocity
+    {
+        get
+        {
+            if (_dictionary.TryGetValue(nameof(Velocity), out var value) && value is Vector vector)
+                return vector;
+
+            return default;
+        }
+    }
 }
 
 public class MoveCommand : ICommand
@@ -22,31 +53,40 @@ public class MoveCommand : ICommand
 }
 
 public interface StartMoveOrder {
-    object ObjectID {get; }
-    object GameID {get; }
+    IDictionary<string, object> GameObject {get;}
     Vector velocity {get; }
 }
 
 public class StartMoveCommand: ICommand{
     private readonly StartMoveOrder _order;
-    public StartMoveCommand(StartMoveOrder order){
-        _order = order;
+    private readonly Queue _queue;
+    public StartMoveCommand(StartMoveOrder order,  Queue queue){
+        _order = order; 
+        _queue = queue;
     }
     public void Execute(){
-        var gameObject = IoC.Resolve<object>("Game.Object.GetById", _order.ObjectID);
-        var setObjectVelocity = IoC.Resolve<ICommand>("Object.SetProperty", "velocity", _order.velocity);
-        if (setObjectVelocity is ICommand setObjectVelocityCommand){
-            setObjectVelocityCommand.Execute(); // !!!
-        }
-        if (gameObject is IMoving MovingObject){var c1 = new MoveCommand(MovingObject);}
-        var inj_obj = new InjectableMacroCommand();
-        var q_obj = IoC.Resolve<object>("Game.Queue");
-        if (q_obj is Queue queue){var c2 = new RepeatCommand(queue, inj_obj);}
-        var c3 = new MCommand(new List<ICommand> { c1, c2 });
-        if (inj_obj is Injectable inj) {inj.Inject(c3);}
+        IMoving MovingGameObject = new MovingAdapter(_order.GameObject);
+
+        var moveCommand = new MoveCommand(MovingGameObject);
         
-        // +write inj to gameObject
-        
-         if (q_obj is Queue queue){queue.Add(inj);}
+        var injectable = new InjectableCommand();
+
+        var repeat = new RepeatCommand(_queue, injectable);
+        var repeatableMove = new MCommand(new List<ICommand> { moveCommand, repeat });
+
+        injectable.Inject(repeatableMove);
+        _order.GameObject["repeatableMove"] = injectable;
+        _queue.Add(repeatableMove);
+    }
+}
+
+public class StopMoveCommand: ICommand{
+    private readonly IDictionary<string, object> _gameObject;
+    public StopMoveCommand(IDictionary<string, object> gameObject){
+        _gameObject = gameObject;
+    }
+    public void Execute(){
+        var injectable = (Injectable)_gameObject["repeatableMove"];
+        injectable.Inject(new EmptyCommand());
     }
 }
