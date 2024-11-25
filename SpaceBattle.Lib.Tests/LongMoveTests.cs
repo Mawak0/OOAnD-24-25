92da8fdc5e346347df6d6db0f6e97c93d44ae9d8
Mawak0 <a32221580@gmail.com>
2024-11-25 20:24:32 +0600

    chore: StopMoveCommandTest added

--- SpaceBattle.Lib.Tests/LongMoveTests.cs
+++ SpaceBattle.Lib.Tests/LongMoveTests.cs
@@ -37,4 +37,37 @@ public class LongMoveTests
 
         Assert.Equal(TargetPosition, game_obj["Position"]);
     }
+
+    [Fact]
+    public void StopMoveCommandTest()
+    {
+        var queue = new Queue();
+
+        var Vector1 = new Mock<IVector>();
+        Vector1.SetupGet(v => v.Values).Returns(new int[] { 1, 1 });
+        var velocity = new Vector(Vector1.Object);
+
+        var Vector2 = new Mock<IVector>();
+        Vector2.SetupGet(v => v.Values).Returns(new int[] { 0, 0 });
+        var Position = new Vector(Vector2.Object);
+
+        var game_obj = new Dictionary<string, object>();
+        game_obj["Position"] = Position;
+
+        var start_move_order = new Mock<StartMoveOrder>();
+        start_move_order.SetupGet(m => m.velocity).Returns(velocity);
+        start_move_order.SetupGet(m => m.GameObject).Returns(game_obj);
+
+        var start_move_cmd = new StartMoveCommand(start_move_order.Object, queue);
+
+        var stop_move_cmd = new StopMoveCommand(game_obj);
+
+        start_move_cmd.Execute();
+
+        queue.Take().Execute();
+        stop_move_cmd.Execute();
+        queue.Take().Execute();
+
+        Assert.Throws<Exception>(() => queue.Take().Execute());
+    }
 }
